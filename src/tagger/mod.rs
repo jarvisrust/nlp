631f2887983db48b571df8f814639197dd2fdbe5
W. Max Lees <william.lees@echostar.com>
2015-07-06 18:21:33 -0600

    Added license agreement

--- src/tagger/mod.rs
+++ src/tagger/mod.rs
@@ -1,3 +1,22 @@
+/**
+   Copyright 2015 W. Max Lees
+
+   This file is part of Jarvis OS.
+
+   Jarvis OS is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Jarvis OS is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Jarvis OS.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 extern crate nalgebra as na;
 
 const N: usize = 86;

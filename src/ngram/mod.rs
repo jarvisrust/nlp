a7b45d538b38fd392a7b36c0fc5070c6d3761c45
W. Max Lees <william.lees@echostar.com>
2015-07-11 18:02:23 -0600

    Added STOP capabilites to ngram

--- src/ngram/mod.rs
+++ src/ngram/mod.rs
@@ -14,6 +14,47 @@ impl NGramSet {
         NGramSet{n: size, set: Vec::<NGram>::new()}
     }
 
+    pub fn create_from_string(size: usize, input: String) -> NGramSet {
+        let mut result = NGramSet::create_empty(size);
+
+        let tokens = tokenizer::tokenize(input);
+
+        // TODO: REPLACE THIS WITH A QUEUE ???
+        let mut temp = NGram::new();
+        for i in tokens.iter() {
+            if temp.len() < size {
+                temp.insert(0, i.clone());
+            }
+
+            if temp.len() == size {
+                temp.reverse();
+                result.push(temp.clone());
+                temp.reverse();
+
+                temp.pop();
+            }
+        }
+
+        result
+    }
+
+    pub fn add_stop(&mut self) {
+        let mut last = match self.set.last() {
+            Some(item) => item.clone(),
+            None => return,
+        };
+
+        for i in 1..last.len() {
+            last.swap(i, i-1);
+        }
+
+        last.pop();
+
+        last.push("STOP".to_string());
+
+        self.set.push(last);
+    }
+
     pub fn pop(&mut self) -> NGram {
         self.set.pop().unwrap()
     }
